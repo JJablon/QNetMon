7f43f8fe37e7f12679ee4b85c27125924a8a65ec
JJablon <jackeneczek@gmail.com>
2017-09-09 19:44:19 +0200

    initial commit

--- QNetMon/Form1.Designer.cs
+++ QNetMon/Form1.Designer.cs
@@ -29,11 +29,55 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "QNetMon";
+            this.notifyIcon1.Visible = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1wifiblack.png");
+            this.imageList1.Images.SetKeyName(1, "2wifigray.png");
+            this.imageList1.Images.SetKeyName(2, "3wifired.png");
+            this.imageList1.Images.SetKeyName(3, "7wifipink.png");
+            this.imageList1.Images.SetKeyName(4, "4wifiyellow.png");
+            this.imageList1.Images.SetKeyName(5, "9wifiviolet.png");
+            this.imageList1.Images.SetKeyName(6, "5wifigreen.png");
+            this.imageList1.Images.SetKeyName(7, "8wifiwhite.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(744, 370);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "QNetMon";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- QNetMon/Form1.cs
+++ QNetMon/Form1.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,89 @@ namespace QNetMon
 {
     public partial class Form1 : Form
     {
+        System.Net.NetworkInformation.Ping p;
+        Bitmap[] icons = null;
+        enum color { black, gray, red, pink, yellow, violet, green, white };
+        IPAddress ip = null;
+        PingReply reply = null;
+        bool error = false;
+        float? rtts = null;
+        long? rtt = null;
+
         public Form1()
         {
+            p = new Ping();
             InitializeComponent();
+            notifyIcon1.Visible = true;
+            this.Focus();
+            this.Hide();
+            this.Visible = false;
+            this.SendToBack();
+            icons = new System.Drawing.Bitmap[Enum.GetNames(typeof(color)).Length];
+            for (int i = 0; i < Enum.GetNames(typeof(color)).Length; i++)
+            {
+                icons[i] = new Bitmap(imageList1.Images[i], new Size(imageList1.Images[i].Width, imageList1.Images[i].Height));
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ip = null;
+            reply = null;
+            error = false;
+            rtts = null;
+            rtt = null;
+            try
+            {
+                ip = new IPAddress(134744072);
+                reply = p.Send(ip);
+                rtts = (float)((float)reply.RoundtripTime / (float)1000f);
+                rtt = reply.RoundtripTime;
+                notifyIcon1.Text = "QNetMon - " + rtt.ToString()+" ms";
+            }
+            catch(Exception )
+            {
+                error = true;
+            }
+          
+
+
+            Bitmap showing = icons[(int)color.white];
+            if (rtt != null && ip != null && reply != null&&error == false)
+            {
+                if (rtt < 100)
+                    showing = icons[(int)color.green];
+                else if (rtt < 200)
+                    showing = icons[(int)color.violet];
+                else if (rtt < 300)
+                    showing = icons[(int)color.yellow];
+                else if (rtt < 500)
+                    showing = icons[(int)color.pink];
+                else if (rtt < 1500)
+                    showing = icons[(int)color.red];
+                else if (rtt < 1500)
+                    showing = icons[(int)color.gray];
+                else
+
+                    showing = icons[(int)color.black];
+
+            }
+            else
+            {
+                //remains default - white
+            }
+            IntPtr icoPtr = showing.GetHicon();// Get an Hicon for myBitmap.
+            Icon ico = Icon.FromHandle(icoPtr);// Create a new icon from the handle.
+            notifyIcon1.Icon = ico;
+
+
         }
     }
 }
